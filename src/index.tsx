5a789f5588056ea922df1678e052ca0416a0782f
fatihpkn <pknfatih@gmail.com>
2021-02-03 19:17:27 +0300

    :heavy_minus_sign: Remove react-snapshot dependency

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,19 @@
 import React from "react";
-import { render } from "react-snapshot";
 import ReactDOM from "react-dom";
+import "react-virtualized/styles.css";
 import App from "./App";
 import "./index.less";
-import "react-virtualized/styles.css";
 
 // import reportWebVitals from "./reportWebVitals";
 
-process.env.NODE_ENV === "development"
-  ? ReactDOM.render(
-      <React.StrictMode>
-        <div className='go-arena-app'>
-          <App />
-        </div>
-      </React.StrictMode>,
-      document.getElementById("root")
-    )
-  : render(
-      <React.StrictMode>
-        <div className='go-arena-app'>
-          <App />
-        </div>
-      </React.StrictMode>,
-      document.getElementById("root")
-    );
+ReactDOM.render(
+  <React.StrictMode>
+    <div className='go-arena-app'>
+      <App />
+    </div>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals())
